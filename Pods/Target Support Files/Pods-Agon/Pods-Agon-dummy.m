4547466e19c7e41f5d47c4f8806382a19a9538a2
gvillaloboz <gavizu@gmail.com>
2018-11-08 10:59:59 +0100

    Adding Podfile and Realm 3.11.1

---
+++ Pods/Target Support Files/Pods-Agon/Pods-Agon-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Agon : NSObject
+@end
+@implementation PodsDummy_Pods_Agon
+@end
---
+++ Pods/Target Support Files/Pods-AgonUITests/Pods-AgonUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AgonUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AgonUITests
+@end
